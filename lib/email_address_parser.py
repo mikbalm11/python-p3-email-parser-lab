0449fc6b1a22693765d872f8f3610f1ff15f1afe
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-09-17 21:54:02 -0400

    passes all tests!

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,11 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+
+    def __init__(self, string):
+        self.string = string
+
+    def parse(self):
+        newList = (re.split(' ,|, | |,', self.string))
+        newList = [x for x in newList if '@' in x]
+        return sorted(set(newList))
